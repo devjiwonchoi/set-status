e33149f5a16e51dd2f4e4e1e31ce36cafee79ac0
devjiwonchoi <devjiwonchoi@gmail.com>
2023-12-31 23:17:13 +0900

    feat: run server through cli bin

---
+++ src/bin/index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { runServer } from '../server'
+
+runServer()
--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,15 @@ import { PROXY_SERVER_PORT } from './constants'
 const app = express()
 app.use('*', proxyMiddleware)
 
-const server = app.listen(parseInt(PROXY_SERVER_PORT), () => {
-  console.log(`Proxy server is running on port ${PROXY_SERVER_PORT}`)
-})
-
-process.on('SIGTERM', () => {
-  console.log('SIGTERM signal received. Shutting down gracefully.')
-  server.close(() => {
-    console.log('HTTP server closed.')
-    process.exit(0)
+export function runServer() {
+  const server = app.listen(parseInt(PROXY_SERVER_PORT), () => {
+    console.log(`Proxy server is running on port ${PROXY_SERVER_PORT}`)
   })
-})
+  process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received. Shutting down gracefully.')
+    server.close(() => {
+      console.log('HTTP server closed.')
+      process.exit(0)
+    })
+  })
+}
