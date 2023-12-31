1b8f1eb9dadc3816b002da4ca8465dae1afc9aa1
devjiwonchoi <devjiwonchoi@gmail.com>
2023-12-31 21:25:56 +0900

    refactor: modify console to state proxy server started

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,9 @@ const app = express()
 app.use('*', proxyMiddleware)
 
 const server = app.listen(parseInt(PROXY_SERVER_PORT), () => {
-  console.log(`Server is running on port ${PROXY_SERVER_PORT}`)
+  console.log(`Proxy server is running on port ${PROXY_SERVER_PORT}`)
 })
 
-// Handle SIGTERM signal
 process.on('SIGTERM', () => {
   console.log('SIGTERM signal received. Shutting down gracefully.')
   server.close(() => {
