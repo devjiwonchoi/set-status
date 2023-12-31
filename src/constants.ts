a5d23aa71d2a636e1ae3e0f051b0bda49bcc4f64
devjiwonchoi <devjiwonchoi@gmail.com>
2023-12-31 23:17:51 +0900

    refactor: module name as set-status

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 export const TARGET_URL = process.env.TARGET_URL ?? 'http://localhost:3000'
-export const TARGET_META_NAME = process.env.TARGET_META_NAME ?? 'next-status'
+export const TARGET_META_NAME = process.env.TARGET_META_NAME ?? 'set-status'
 export const PROXY_SERVER_PORT = process.env.PROXY_SERVER_PORT ?? '3001'
 export const IGNORE_PATH_REGEX =
   process.env.IGNORE_PATH_REGEX ??
